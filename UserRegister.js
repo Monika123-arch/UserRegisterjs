66c40092e7b5c6ddaa9e6f6fb637b5fe4540d9cc
Monika <monikadkale98@gmail.com>
2021-04-19 17:03:33 +0530

    added last name

--- UserRegister.js
+++ UserRegister.js
@@ -1,10 +1,48 @@
-//UC1
-const prompt = require('prompt-sync')({sigint: true}); //user input
-const pattern = require('./constant')
+// //UC1
+// const prompt = require('prompt-sync')({sigint: true}); //user input
+// const pattern = require('./constant')
+// var regPattern;
+// let namePattern = "^[A-Z]{1}[a-z]{3,}";
+
+// validation = function(data,pattern){          
+//     regPattern = new RegExp(pattern);
+//     result = regPattern.test(data);
+//     if(result == true){
+//         console.log("Validated.");
+//     }
+//     else {
+//         console.log("Please enter correct format!");
+//         return false;
+//     }
+// }
+
+// let getData = function(string, pattern){                 //getData=function
+//     let readData = prompt(string);
+//     let result = validation(readData,pattern);
+  
+//     while(result == false){
+//         if(result == true){
+//             return true; 
+//         }
+//         else{
+//             let readData = prompt(string);
+//             let result = validation(readData,pattern);
+//             return result;
+//         }
+       
+//     }
+// }
+
+// getData('Enter the first name:',namePattern);
+// console.log('welcome User Registration problem');
+
+//UC2
+
+const prompt = require('prompt-sync')({sigint: true}); 
 var regPattern;
 let namePattern = "^[A-Z]{1}[a-z]{3,}";
 
-validation = function(data,pattern){          
+validation = function(data,pattern){
     regPattern = new RegExp(pattern);
     result = regPattern.test(data);
     if(result == true){
@@ -16,7 +54,8 @@ validation = function(data,pattern){
     }
 }
 
-let getData = function(string, pattern){                 //getData=function
+
+let getData = function(string, pattern){
     let readData = prompt(string);
     let result = validation(readData,pattern);
   
@@ -34,5 +73,4 @@ let getData = function(string, pattern){                 //getData=function
 }
 
 getData('Enter the first name:',namePattern);
-console.log('welcome User Registration problem');
-
+getData('Enter the last name:',namePattern);
