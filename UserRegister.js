efcabaaaf0c477dd0183f19e69bc5381fcf1049b
Monika <monikadkale98@gmail.com>
2021-04-19 17:18:03 +0530

    added email id

--- UserRegister.js
+++ UserRegister.js
@@ -1,46 +1,7 @@
-// //UC1
-// const prompt = require('prompt-sync')({sigint: true}); //user input
-// const pattern = require('./constant')
-// var regPattern;
-// let namePattern = "^[A-Z]{1}[a-z]{3,}";
-
-// validation = function(data,pattern){          
-//     regPattern = new RegExp(pattern);
-//     result = regPattern.test(data);
-//     if(result == true){
-//         console.log("Validated.");
-//     }
-//     else {
-//         console.log("Please enter correct format!");
-//         return false;
-//     }
-// }
-
-// let getData = function(string, pattern){                 //getData=function
-//     let readData = prompt(string);
-//     let result = validation(readData,pattern);
-  
-//     while(result == false){
-//         if(result == true){
-//             return true; 
-//         }
-//         else{
-//             let readData = prompt(string);
-//             let result = validation(readData,pattern);
-//             return result;
-//         }
-       
-//     }
-// }
-
-// getData('Enter the first name:',namePattern);
-// console.log('welcome User Registration problem');
-
-//UC2
-
 const prompt = require('prompt-sync')({sigint: true}); 
 var regPattern;
 let namePattern = "^[A-Z]{1}[a-z]{3,}";
+let emailReg = "^[a-zA-Z0-9_.]+@[a-zA-Z.a-zA-Z{2,}.a-zA-Z{2,}]+$";
 
 validation = function(data,pattern){
     regPattern = new RegExp(pattern);
@@ -74,3 +35,6 @@ let getData = function(string, pattern){
 
 getData('Enter the first name:',namePattern);
 getData('Enter the last name:',namePattern);
+getData('Enter the email:',emailReg);
+
+
